9c7a12d06937d2ae9bb89905f6e60bd5a9fd6212
mayevskyivan5@gmail.com <mayevskyivan5@gmail.com>
2021-12-19 22:30:25 +0200

    create Database

---
+++ ASP.TestProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ASP.TestProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.TestProject.Data;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+
+}
--- ASP.TestProject/Program.cs
+++ ASP.TestProject/Program.cs
@@ -1,7 +1,28 @@
+using ASP.TestProject.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+
+//builder.Services.AddDbContext<AppEFContext>(options =>
+//               options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//builder.Services.AddIdentity<AppUser, AppRole>(options =>
+//{
+//    options.Stores.MaxLengthForKeys = 128;
+//    options.Password.RequireDigit = false;
+//    options.Password.RequiredLength = 5;
+//    options.Password.RequireNonAlphanumeric = false;
+//    options.Password.RequireUppercase = false;
+//    options.Password.RequireLowercase = false;
+//})
+//   .AddEntityFrameworkStores<AppEFContext>();
 
 var app = builder.Build();
 
