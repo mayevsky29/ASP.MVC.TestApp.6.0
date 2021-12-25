8ff7258bbf702a0c4c871ba6d7c3551158e38820
mayevskyivan5@gmail.com <mayevskyivan5@gmail.com>
2021-12-25 17:11:05 +0200

    add crud

--- ASP.TestProject/Controllers/CategoryController.cs
+++ ASP.TestProject/Controllers/CategoryController.cs
@@ -41,5 +41,72 @@ public class CategoryController : Controller
         }
         return View(obj);
     }
+
+    // GET
+    public IActionResult Edit(int? id)
+    {
+        if(id == null || id == 0)
+        {
+            return NotFound();
+        }
+        var categoryFromDb = _db.Categories.Find(id);
+        //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id==id);
+        //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+        if (categoryFromDb == null)
+        {
+            return NotFound();
+        }
+
+        return View(categoryFromDb);
+    }
+
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category obj)
+    {
+        if (obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+        }
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+
+    public IActionResult Delete(int? id)
+    {
+        if(id == 0 || id == 0)
+            {
+                return NotFound();
+            }
+        var CategoryFromDb = _db.Categories.Find(id);
+        if (CategoryFromDb == null)
+        {
+            return NotFound();
+        }
+
+        return View(CategoryFromDb);
+    }
+
+    // POST
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeletePost(int? id)
+    {
+        var obj = _db.Categories.Find(id);
+        if(obj == null)
+        {
+            return NotFound();
+        }
+        _db.Categories.Remove(obj);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
 }
 
